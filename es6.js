efaf186bd04600bb5a2ce3a21b8119e24a618a33
Abhishek <abmishra700@gamil.com>
2021-03-14 01:47:42 +0530

    array in js topic traversal of an array

--- es6.js
+++ es6.js
@@ -1,119 +1,196 @@
-//  ************************  Modern javascript  ****************************//
+ ************************  Modern javascript  ****************************//
 
-//LET and CONST //
+LET and CONST //
 
-//  var => function scope
-//  let and const => block scope
+ var => function scope
+ let and const => block scope
 
-//  var myname = "abhsiehk"
-//  console.log(myname);
+ var myname = "abhsiehk"
+ console.log(myname);
 
-//  myname = "mishra"
-//  console.log(myname);
+ myname = "mishra"
+ console.log(myname);
 
-// let myname = "abhsiehk"
-// console.log(myname);
+let myname = "abhsiehk"
+console.log(myname);
 
-// myname = "mishra"
-// console.log(myname);
+myname = "mishra"
+console.log(myname);
 
-// const myname = "abhsiehk"
-// console.log(myname);
+const myname = "abhsiehk"
+console.log(myname);
 
-// myname = "mishra"
-// console.log(myname);
+myname = "mishra"
+console.log(myname);
 
-// function bio() {
-//   const myfirstname = "abhsiehk";
-//   console.log(myfirstname);
+function bio() {
+  const myfirstname = "abhsiehk";
+  console.log(myfirstname);
 
-//   if (true) {
-//     const mylastname = "mishar";
-//     console.log("inner " + mylastname);
-//     console.log("inner " + myfirstname);
-//   }
-//   console.log("innerouter " + mylastname);
-// }
+  if (true) {
+    const mylastname = "mishar";
+    console.log("inner " + mylastname);
+    console.log("inner " + myfirstname);
+  }
+  console.log("innerouter " + mylastname);
+}
 
-// bio();
+bio();
 
-// template literals (temaplate strings)
+template literals (temaplate strings)
 
-// let person = "abhi"
-// let age = "200"
+let person = "abhi"
+let age = "200"
 
-// function myTag( strings , personexp, ageexp ){
-//     let str0 = strings[0]; // "that"
-//     let str1 = strings[1];// " is a"
-//     let str2 = strings[2];//"."
+function myTag( strings , personexp, ageexp ){
+    let str0 = strings[0]; // "that"
+    let str1 = strings[1];// " is a"
+    let str2 = strings[2];//"."
 
-//     let agestr;
-//     if(ageexp > 99){
-//         agestr = "centenrian"
+    let agestr;
+    if(ageexp > 99){
+        agestr = "centenrian"
 
-//     }else{
-//         agestr = "youngster"
-//     }
+    }else{
+        agestr = "youngster"
+    }
 
-//     return `${str0} ${personexp}${str1}${agestr}${str2}`;
-// }
+    return `${str0} ${personexp}${str1}${agestr}${str2}`;
+}
 
-// let output = myTag`That ${person} is a ${age}.`;
+let output = myTag`That ${person} is a ${age}.`;
 
-// console.log(output);
+console.log(output);
 
-// default parameters
-// default function parameters allow named parameters to be 
-// initialized with default values if no value or undefined is passed 
-
-
-// function multi (a ,b){
-//   return a * b;
-// }
-
-// console.log(multi(2,3));
+default parameters
+default function parameters allow named parameters to be 
+initialized with default values if no value or undefined is passed 
 
 
-// function test ( num = 1){
-//     console.log(typeof num)
-// }
+function multi (a ,b){
+  return a * b;
+}
 
-// test(); // "num" ( num is set to 1 )
-// test(undefined); // ""
-
-// function multi(a ,b=2){
-//     debugger;
-//     return a*b;
-// }
-// console.log(multi(3));
-
-// ******************************** fat arror function ********************************////////
-
-// An arrow function expression is a compact alternative to a traditional function expression, but is limited and can't be used in all situations.
-
-// Differences & Limitations:
-
-// Does not have its own bindings to this or super, and should not be used as methods.
-// Does not have arguments, or new.target keywords.
-// Not suitable for call, apply and bind methods, which generally rely on establishing a scope.
-// Can not be used as constructors.
-// Can not use yield, within its body.
+console.log(multi(2,3));
 
 
+function test ( num = 1){
+    console.log(typeof num)
+}
 
-//  const  sum = () => ` the sum of the two number is ${(a=5)+(b=3)} `;
-// console.log(sum())
+test(); // "num" ( num is set to 1 )
+test(undefined); // ""
 
-// const materials = [
-//     "abhi",
-//     "sachin",
-//     "hritik",
-//     "vikrant",
-//     "fiza",
-//     "anjali",
-//     "abhishekta",
-//     "diyali",
-//     "ankita"
-// ]
-// console.log(materials.map(materials => materials.length));
+function multi(a ,b=2){
+    debugger;
+    return a*b;
+}
+console.log(multi(3));
 
+******************************** fat arror function ********************************////////
+
+An arrow function expression is a compact alternative to a traditional function expression, but is limited and can't be used in all situations.
+
+Differences & Limitations:
+
+Does not have its own bindings to this or super, and should not be used as methods.
+Does not have arguments, or new.target keywords.
+Not suitable for call, apply and bind methods, which generally rely on establishing a scope.
+Can not be used as constructors.
+Can not use yield, within its body.
+
+
+
+ const  sum = () => ` the sum of the two number is ${(a=5)+(b=3)} `;
+console.log(sum())
+
+const materials = [
+    "abhi",
+    "sachin",
+    "hritik",
+    "vikrant",
+    "fiza",
+    "anjali",
+    "abhishekta",
+    "diyali",
+    "ankita"
+]
+console.log(materials.map(materials => materials.length));
+
+************* arrays in javascripts *********************
+
+when we are use var , we can stored only one valuse at a time
+
+var myFriend1 ="hritik"
+var myFriend1 ="sachin"
+var myFriend1 ="vikrant"
+
+it is not go  things that was implemented call many times var for one name 
+so we have arrays for that
+
+when we feel like storing multiple values in one variable then instead of var ,
+we will use ARRAY
+
+arrays is class in js
+
+
+
+var myFriends = ["abhsihek", ' sachin', "hritik"];
+console.log(myFriends.length)
+console.log(myFriends.length -1)
+
+myFriends.forEach(function(name, index) {
+    console.log(name, index)
+}
+
+
+
+Topic in array
+
+ARRAY traversal in array
+navigate through an array
+if we want to get  the single data at  a time and also
+if we want to change the data ................
+
+var array = ['abhi','mishra','sachin','singh'];
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+if we want to check the length of elements of an array use length property
+console.log(array.length);
+last  value of array to check to use -1
+console.log(array[array.length - 1]);
+we use for loop to navigate
+for (var i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+
+
+AFter es6 we have for in and for of  loop to ******
+var array = ['abhi','mishra','sachin','singh','Apple', 'Banana',"Orange","Strawberry", "Banana", "Mango"];
+for(let elements in array) {
+    console.log(elements);
+}
+for(let elements of array) {
+    console.log(elements);
+}
+
+
+Array.prototype.foreach()
+calls a  function for each element in the array
+
+var array = ['abhi','mishra','sachin','singh','Apple',
+ 'Banana',"Orange","Strawberry", "Banana", "Mango"];
+
+ foreach is combination of for in and for of
+ array.forEach(function(element,index,array){
+    console.log(element + " index :" + index )
+ });
+
+fat arrow function
+ array.forEach((element,index,array) => {
+    console.log(element + " index :" + index )
+ });
+
+ 
--- index.js
+++ index.js
@@ -1,403 +1,403 @@
-// 1--> variable and values in js
+1--> variable and values in js
 
-// var myname = 'abhishek mishra';
-// console.log(myname)
+var myname = 'abhishek mishra';
+console.log(myname)
 
-// var mylist ="mongo, banna, samosa";
-// var myAge =28;
-// console.log(mylist , myAge)
+var mylist ="mongo, banna, samosa";
+var myAge =28;
+console.log(mylist , myAge)
 
-// var $myname = 'abhiiii'
-// console.log($myname)
+var $myname = 'abhiiii'
+console.log($myname)
 
-// 2--> DATA TYPES IN JS
+2--> DATA TYPES IN JS
 
-// var myname ='abhishek mishra ';
-// console.log(typeof(myname));
+var myname ='abhishek mishra ';
+console.log(typeof(myname));
 
-// var myage =24;
-// console.log(typeof(myage))
+var myage =24;
+console.log(typeof(myage))
 
-// var iamabhi =  true;
-// console.log(typeof(iamabhi));
+var iamabhi =  true;
+console.log(typeof(iamabhi));
 
-// var num ="abhi";
-// console.log(num);
-// console.log(typeof(num));
+var num ="abhi";
+console.log(num);
+console.log(typeof(num));
 
-// var num =   'abhi' + 'mishra';
-// console.log( 2 - '6');
-// console.log(typeof(num));
+var num =   'abhi' + 'mishra';
+console.log( 2 - '6');
+console.log(typeof(num));
 
-// interview question//////////////////////////////////////////
-// Difference between null vs undefined ?
+interview question//////////////////////////////////////////
+Difference between null vs undefined ?
 
-// var iamuseless =null;
-// console.log(null);
-// console.log(typeof(iamuseless));
+var iamuseless =null;
+console.log(null);
+console.log(typeof(iamuseless));
 
-// var iamstandby;
-// console.log(iamstandby);
+var iamstandby;
+console.log(iamstandby);
 
-// undefiend also knows as datatypes
-// interview question
+undefiend also knows as datatypes
+interview question
 
-// what is Nan ??
-// console.log('abhi' - 'mishra');
+what is Nan ??
+console.log('abhi' - 'mishra');
 
-// var mynumber =36782687;
-// var myname= 'abhi'
-// console.log(isNaN(myname));
-// console.log(isNaN(mynumber));
+var mynumber =36782687;
+var myname= 'abhi'
+console.log(isNaN(myname));
+console.log(isNaN(mynumber));
 
-// console.log(NaN === NaN);
-// console.log(Number.NaN === NaN);
-// console.log(isNaN(NaN));
-// console.log(Number.isNaN(NaN));
+console.log(NaN === NaN);
+console.log(Number.NaN === NaN);
+console.log(isNaN(NaN));
+console.log(Number.isNaN(NaN));
 
-// --> expression and operators
-// 5 is called operand
-// + is called operator
-// // combination of both is called expression
-// var x = 5;
-// var y = 5;
-// console.log(x == y);
+--> expression and operators
+5 is called operand
++ is called operator
+// combination of both is called expression
+var x = 5;
+var y = 5;
+console.log(x == y);
 
-// console.log(5+20);
+console.log(5+20);
 
-// ////////////////////// Arithimatic operators /////////////////
+////////////////////// Arithimatic operators /////////////////
 
-// console.log(3+5);
-// console.log(3-5);
-// console.log(3*5);
-// console.log(3/5);
+console.log(3+5);
+console.log(3-5);
+console.log(3*5);
+console.log(3/5);
 
-// // % is called moduls operator
-// console.log("Remainder Operator "+ 27%4);
+// % is called moduls operator
+console.log("Remainder Operator "+ 27%4);
 
-// incremenet and decrement operator/////////////////////////////
-// postfix , if used postfix , with operator after operand for expamle , x++
-// var num = 15;
-// var newNum = num++ + 5 ;
-// console.log(num);
-// console.log(newNum);
+incremenet and decrement operator/////////////////////////////
+postfix , if used postfix , with operator after operand for expamle , x++
+var num = 15;
+var newNum = num++ + 5 ;
+console.log(num);
+console.log(newNum);
 
-// prefix
-// var num = 15;
-// var newNum = ++num + 5;
-// console.log(num);
-// console.log(newNum);
+prefix
+var num = 15;
+var newNum = ++num + 5;
+console.log(num);
+console.log(newNum);
 
-// postfix decrement
-// var num = 15;
-// var newNum = num-- + 5;
-// console.log(num);
-// console.log(newNum);
+postfix decrement
+var num = 15;
+var newNum = num-- + 5;
+console.log(num);
+console.log(newNum);
 
-// // pretfix decrement
-// var num = 15;
-// var newNum = --num + 5;
-// console.log(num);
-// console.log(newNum);
+// pretfix decrement
+var num = 15;
+var newNum = --num + 5;
+console.log(num);
+console.log(newNum);
 
-// ////////comparison operators////////////
+////////comparison operators////////////
 
-// var a= 90;
-// var b=100;
-// equal
-// console.log(a == b);
-// not equal
-// console.log(a !== b);
-// greater than
-// console.log(a > b);
-// greater than equal
-// console.log(a >= b);
-// less then
-// console.log(a < b);
-// less than equal
-// console.log(a <= b);
+var a= 90;
+var b=100;
+equal
+console.log(a == b);
+not equal
+console.log(a !== b);
+greater than
+console.log(a > b);
+greater than equal
+console.log(a >= b);
+less then
+console.log(a < b);
+less than equal
+console.log(a <= b);
 
-// /////// logical operators//////
-// AND (&&)
-// t and t = t
-// f and t = f
-// t and f  = f
-// var a = 10;
-// var b = -30;
-// console.log (a > b && b < 0);
-// OR ( || )
-// t or f = t
-// f or f = f
-// f or t = t
-// console.log ( (a < b) || ( b < -30 ) );
-// console.log(!false);
+/////// logical operators//////
+AND (&&)
+t and t = t
+f and t = f
+t and f  = f
+var a = 10;
+var b = -30;
+console.log (a > b && b < 0);
+OR ( || )
+t or f = t
+f or f = f
+f or t = t
+console.log ( (a < b) || ( b < -30 ) );
+console.log(!false);
 
-// string operators ///////////
+string operators ///////////
 
-// console.log("hello " + "world");
+console.log("hello " + "world");
 
-// var myname = "abhishek"
-// console.log(myname + " mishra");
-// console.log(myname + " thapa");
-// console.log(myname + " kohli");
+var myname = "abhishek"
+console.log(myname + " mishra");
+console.log(myname + " thapa");
+console.log(myname + " kohli");
 
-// console.log(3**3);
+console.log(3**3);
 
-// console.log(78 - "abhi");
+console.log(78 - "abhi");
 
-// var a =10;
-// var b = 20;
-// // // c = b;
-// // // b = a;
-// // // a = c;
-// // // console.log(a);
-// // // console.log(b);
+var a =10;
+var b = 20;
+// // c = b;
+// // b = a;
+// // a = c;
+// // console.log(a);
+// // console.log(b);
 
-// a = a + b ; // a = 30
-// b = a - b; // b = 10
-// a = a - b; // a = 10
-// console.log(a);
-// console.log(b);
+a = a + b ; // a = 30
+b = a - b; // b = 10
+a = a - b; // a = 10
+console.log(a);
+console.log(b);
 
-// interview question
-// what is Difference between == and ===
-// var num1 = 5
-// var num2 = '5';
-// console.log(num1);
-// console.log(num2);
-// console.log(num1 === num2);
+interview question
+what is Difference between == and ===
+var num1 = 5
+var num2 = '5';
+console.log(num1);
+console.log(num2);
+console.log(num1 === num2);
 
-// ******* Control Statement */
-// var tomr = 'sunny';
+******* Control Statement */
+var tomr = 'sunny';
 
-// if(tomr == 'rain'){
-//   console.log('take raincoat');
-// }else{
-//   console.log("no take raincoat");
+if(tomr == 'rain'){
+  console.log('take raincoat');
+}else{
+  console.log("no take raincoat");
+}
+
+var year = 2020;
+debugger;
+if(year % 4 === 0){
+  if(year % 100 === 0){
+     if(year % 400 === 0){
+         console.log("the year"+ year + " is a leap year")
+     }else{
+        console.log("the year"+ year + "is not a leap year")
+     }
+  }else{
+       console.log("the year" + year + "is a leap year")
+  }
+}else{
+    console.log("the year "+ year + "is not a leap year")
+}
+
+what is the truthy and falsy value in javascript
+ we have 5 total falsy values in javascript
+0 , "" ,  undefined , null , NaN false ,
+
+if (NaN){
+    console.log("ya we won the game")
+}else{
+    console.log("we loss the game")
+}
+
+condition ( ternary) operators //////
+the conditional ternary is the only javascript operator
+that takes three operands
+
+var age = 18;
+
+if(age >= 18){
+    console.log("you are eligible to vote");
+}else{
+    console.log("you are not eligible to vote");
+}
+this is called ternary
+var age = 17;
+console.log((age > 18) ? "you can eligible to vote" : "you are not eligible to vote");
+
+            switch Statement /////////////////
+evaluates an  expression matching the expression value to a
+case clause and exexutes statements associated with that case.
+
+find the area of circle triangle and rectangles
+
+var area = "circle";
+var PI = 3.142,
+  l = 5,
+  b = 4,
+  r = 3;
+if (area == "circle") {
+  console.log("the area of the circle is :" + PI * r ** 2);
+} else if (area == "triangle") {
+  console.log("the area of the triangle is :" + (l * b) / 2);
+} else if (area == "rectangle") {
+  console.log("the area of the rectangle is :" + l * b);
+} else {
+  console.log("please enter valid data");
+}
+thisisme ko This_is_me karo aisa kuch hai yeh
+var input = "ThisISMe";
+console.log(input.substring(0,4).toLowerCase()+"_"+input.substring(2,4).toLowerCase()+"_"+input.substring(6,8).toLowerCase());
+
+var area = "circle";
+var PI = 3.142,
+  l = 5,
+  b = 4,
+  r = 3;
+switch (area) {
+  case "circle":
+    console.log("the area of the circle is :" + PI * r ** 2);
+    break;
+  case "trianlge":
+    console.log("the area of the triangle is :" + (l * b) / 2);
+    break;
+  case "rectangle":
+    console.log("the area of the rectangle is :" + l * b);
+    break;
+  default:
+    console.log("please enter valid data");
+}
+
+we have break statement matlab khatm
+Terminates the current loop , switch , or label
+statement and transfers
+program control to the statement following the break statement
+
+
+            while loop statement////////////////////
+the while loop statement creates a loop that executes a specified statement
+as long as the test condition evaluates to true
+
+var num =0;
+// block scope
+while(num <= 10){
+    console.log(num); // infinite loop this is called
+    num++;
+}
+
+Do-while loop statement
+
+var num = 0;
+debugger;
+do{
+    console.log(num);
+    num++; 
+}while(num <= 10);
+
+*******for loop*******////////
+
+for(intilizer; condition ; iteration)
+{
+
+}
+
+Statement 1 sets a variable before the loop starts (var num = 0).
+
+Statement 2 defines the condition for the loop to run (i must be less than 10).
+
+Statement 3 increases a value (num++) each time the code block in the loop has been executed.
+
+for(var num = 0; num < 10; num++){
+    debugger;
+ console.log(num);
+}
+
+for (var i = 0; i < 5; i++) {
+ console.log(i)
+  }
+
+for ( var int = 0 ; int <= 10; int++)
+{
+    console.log(int)
+}
+
+for(var num=1; num<= 10; num++)
+{
+    var table = 15;
+    console.log(table + " * " + num + " =" + (table*num));
 // }
+// * javascript functions *///////////////////////
 
-// var year = 2020;
-// debugger;
-// if(year % 4 === 0){
-//   if(year % 100 === 0){
-//      if(year % 400 === 0){
-//          console.log("the year"+ year + " is a leap year")
-//      }else{
-//         console.log("the year"+ year + "is not a leap year")
-//      }
-//   }else{
-//        console.log("the year" + year + "is a leap year")
-//   }
-// }else{
-//     console.log("the year "+ year + "is not a leap year")
-// }
+& what we will see in function
+~function definition
 
-// what is the truthy and falsy value in javascript
-//  we have 5 total falsy values in javascript
-// 0 , "" ,  undefined , null , NaN false ,
+* JavaScript functions are defined with the function keyword.
+* A javascript function is a block of code that designed to perform a particular taks.
+* You can use a function declaration or a function expression.
 
-// if (NaN){
-//     console.log("ya we won the game")
-// }else{
-//     console.log("we loss the game")
-// }
+var a = 10;
+var b = 20;
+var sum = a + b;
+console.log(sum)
 
-// condition ( ternary) operators //////
-// the conditional ternary is the only javascript operator
-// that takes three operands
+function sum(){
+    var a = 290, b = 29289289;
+    var total = a + b;
+    console.log(total); 
+}
+~Calling a function
+calling the  function
+defining fun does not work it
+a javascript fun is executed when "something invokes it (calls it)"
+sum();
+~function parameter
+! function parameters are  the names listed in the functions definition
+~function Arguments
+! function arguments are the real values passed to the function
 
-// var age = 18;
-
-// if(age >= 18){
-//     console.log("you are eligible to vote");
-// }else{
-//     console.log("you are not eligible to vote");
-// }
-// this is called ternary
-// var age = 17;
-// console.log((age > 18) ? "you can eligible to vote" : "you are not eligible to vote");
-
-//             switch Statement /////////////////
-// evaluates an  expression matching the expression value to a
-// case clause and exexutes statements associated with that case.
-
-// find the area of circle triangle and rectangles
-
-// var area = "circle";
-// var PI = 3.142,
-//   l = 5,
-//   b = 4,
-//   r = 3;
-// if (area == "circle") {
-//   console.log("the area of the circle is :" + PI * r ** 2);
-// } else if (area == "triangle") {
-//   console.log("the area of the triangle is :" + (l * b) / 2);
-// } else if (area == "rectangle") {
-//   console.log("the area of the rectangle is :" + l * b);
-// } else {
-//   console.log("please enter valid data");
-// }
-// thisisme ko This_is_me karo aisa kuch hai yeh
-// var input = "ThisISMe";
-// console.log(input.substring(0,4).toLowerCase()+"_"+input.substring(2,4).toLowerCase()+"_"+input.substring(6,8).toLowerCase());
-
-// var area = "circle";
-// var PI = 3.142,
-//   l = 5,
-//   b = 4,
-//   r = 3;
-// switch (area) {
-//   case "circle":
-//     console.log("the area of the circle is :" + PI * r ** 2);
-//     break;
-//   case "trianlge":
-//     console.log("the area of the triangle is :" + (l * b) / 2);
-//     break;
-//   case "rectangle":
-//     console.log("the area of the rectangle is :" + l * b);
-//     break;
-//   default:
-//     console.log("please enter valid data");
-// }
-
-// we have break statement matlab khatm
-// Terminates the current loop , switch , or label
-// statement and transfers
-// program control to the statement following the break statement
+//    ^ fun parameter vs fun arguments
+function sum(a , b){  //!parameter
+    var total = a + b;
+    console.log(total); 
+}
+sum(28998,88779898); //!arguments
+sum(23,4); //!arguments
 
 
-//             while loop statement////////////////////
-// the while loop statement creates a loop that executes a specified statement
-// as long as the test condition evaluates to true
+// ~function expressions
+function expression simply means
+create a function and put it into the variable
+jab bhi app function ko call kar rahe ho to app ek variable main usko put kardo
 
-// var num =0;
-// // block scope
-// while(num <= 10){
-//     console.log(num); // infinite loop this is called
-//     num++;
-// }
-
-// Do-while loop statement
-
-// var num = 0;
-// debugger;
-// do{
-//     console.log(num);
-//     num++; 
-// }while(num <= 10);
-
-// *******for loop*******////////
-
-// for(intilizer; condition ; iteration)
-// {
-
-// }
-
-// Statement 1 sets a variable before the loop starts (var num = 0).
-
-// Statement 2 defines the condition for the loop to run (i must be less than 10).
-
-// Statement 3 increases a value (num++) each time the code block in the loop has been executed.
-
-// for(var num = 0; num < 10; num++){
-//     debugger;
-//  console.log(num);
-// }
-
-// for (var i = 0; i < 5; i++) {
-//  console.log(i)
-//   }
-
-// for ( var int = 0 ; int <= 10; int++)
-// {
-//     console.log(int)
-// }
-
-// for(var num=1; num<= 10; num++)
-// {
-//     var table = 15;
-//     console.log(table + " * " + num + " =" + (table*num));
-// // }
-// // * javascript functions *///////////////////////
-
-// & what we will see in function
-// ~function definition
-
-// * JavaScript functions are defined with the function keyword.
-// * A javascript function is a block of code that designed to perform a particular taks.
-// * You can use a function declaration or a function expression.
-
-// var a = 10;
-// var b = 20;
-// var sum = a + b;
-// console.log(sum)
-
-// function sum(){
-//     var a = 290, b = 29289289;
-//     var total = a + b;
-//     console.log(total); 
-// }
-// ~Calling a function
-// calling the  function
-// defining fun does not work it
-// a javascript fun is executed when "something invokes it (calls it)"
-// sum();
-// ~function parameter
-// ! function parameters are  the names listed in the functions definition
-// ~function Arguments
-// ! function arguments are the real values passed to the function
-
-// //    ^ fun parameter vs fun arguments
-// function sum(a , b){  //!parameter
-//     var total = a + b;
-//     console.log(total); 
-// }
-// sum(28998,88779898); //!arguments
-// sum(23,4); //!arguments
+function sum(a,b){
+    var total = a+ b;
+    console.log(total);
+}
+var funexp = sum(20,10); // this is called fun exp
+funexp;
 
 
-// // ~function expressions
-// function expression simply means
-// create a function and put it into the variable
-// jab bhi app function ko call kar rahe ho to app ek variable main usko put kardo
+// ~return keyword
+when javascript reaches a return statement
+the function will be stop executing
 
-// function sum(a,b){
-//     var total = a+ b;
-//     console.log(total);
-// }
-// var funexp = sum(20,10); // this is called fun exp
-// funexp;
+function sum(a,b){
+   return total = a+b;
+}
+var funexp = sum(20,10); // this is called fun exp
+console.log(funexp);
 
 
-// // ~return keyword
-// when javascript reaches a return statement
-// the function will be stop executing
+// ~Anonymons function
+a functions is a without name is called a anonymous function
 
-// function sum(a,b){
-//    return total = a+b;
-// }
-// var funexp = sum(20,10); // this is called fun exp
-// console.log(funexp);
-
-
-// // ~Anonymons function
-// a functions is a without name is called a anonymous function
-
-// var funxep = function(a,b){
-//     debugger;
-//     return total =a+b;
-// }
-// var sum = funxep(82,72)
-// var sum1 = funxep(82,70)
-// console.log("sum of first function " + sum)
-// console.log("sun of second function " + sum1)
-// console.log("total of first and second functions " + (sum +sum1))
-// console.log("first greater than second" + sum > sum1);
+var funxep = function(a,b){
+    debugger;
+    return total =a+b;
+}
+var sum = funxep(82,72)
+var sum1 = funxep(82,70)
+console.log("sum of first function " + sum)
+console.log("sun of second function " + sum1)
+console.log("total of first and second functions " + (sum +sum1))
+console.log("first greater than second" + sum > sum1);
 
 
-// interviwes question 
-// why use function
-// you can reuse the code many times and with different arguments 
-// produce different results
+interviwes question 
+why use function
+you can reuse the code many times and with different arguments 
+produce different results
 
-// DRY => do not repeat yourself
+DRY => do not repeat yourself
